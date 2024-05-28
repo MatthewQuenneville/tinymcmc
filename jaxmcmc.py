3ccfa385c5b929be71e1e6beb4797881613b145d
Matthew <mattq1@gmail.com>
2024-05-27 20:25:15 -0400

    Fix bug where some input shapes cause errors

--- jaxmcmc.py
+++ jaxmcmc.py
@@ -5,18 +5,31 @@ import matplotlib.pyplot as plt
 from functools import partial
 
 @partial(jax.jit, static_argnames=('E_dist'))
-def step_rwm(key, E_dist, samples, proposal_std):
+def step_rwm(key, E_dist, samples, proposal_std, bounds=(-jnp.inf, jnp.inf)):
     key_step, key_accept = jrandom.split(key, 2)
     proposal = samples+jrandom.normal(key_step, shape=samples.shape)*proposal_std
+
+    # Metropolis-Hastings acceptance/Rejection
     alpha = jnp.exp(E_dist(samples)-E_dist(proposal))
     alpha = jnp.minimum(alpha,1.)
     accept = jrandom.uniform(key_accept, minval=0, maxval=1, shape = alpha.shape)<alpha
+
+    # Enforce bound constraints
+    out_of_bounds = jnp.logical_or(proposal>bounds[1],
+                                   proposal<bounds[0])
+    if len(samples.shape)>1:
+        out_of_bounds = jnp.any(out_of_bounds, axis=1)
+    accept = jnp.where(out_of_bounds, False, accept)
+    if len(samples.shape)>1:
+        accept = jnp.repeat(accept[:,jnp.newaxis],samples.shape[1],axis=1)
     samples = jnp.where(accept, proposal, samples)
     return samples
 
 @partial(jax.jit, static_argnames=('E_dist'))
-def step_mala(key, E_dist, samples, proposal_std):
+def step_mala(key, E_dist, samples, proposal_std, bounds=(-jnp.inf, jnp.inf)):
+    
     E_grad = jax.vmap(jax.grad(E_dist))
+
     key_step, key_accept = jrandom.split(key, 2)
     proposal = samples
     proposal = proposal - 0.5*proposal_std**2*E_grad(samples)
@@ -25,119 +38,58 @@ def step_mala(key, E_dist, samples, proposal_std):
     def E_q(xp, x):
         return 1/2/proposal_std**2*jnp.einsum('i...->i',(xp-x+0.5*proposal_std**2*E_grad(x))**2)
 
+    # Metropolis-Hastings acceptance/Rejection
     log_alpha = E_dist(samples) - E_dist(proposal) - (E_q(samples, proposal) - E_q(proposal, samples))
     alpha = jnp.exp(jnp.minimum(log_alpha,0.))
     accept = jrandom.uniform(key_accept, minval=0, maxval=1, shape = alpha.shape)<alpha
+
+    # Enforce bound constraints
+    out_of_bounds = jnp.logical_or(proposal>bounds[1],
+                                   proposal<bounds[0])
+    if len(samples.shape)>1:
+        out_of_bounds = jnp.any(out_of_bounds, axis=1)
+    accept = jnp.where(out_of_bounds, False, accept)
+    if len(samples.shape)>1:
+        accept = jnp.repeat(accept[:,jnp.newaxis],samples.shape[1],axis=1)
     samples = jnp.where(accept, proposal, samples)
     return samples
     
 @partial(jax.jit, static_argnames=('E_dist', 'L'))
-def step_hmc(key, E_dist, samples, length_scale, L = 10, M=1.):
+def step_hmc(key, E_dist, samples, length_scale, L = 10, M=1., bounds=(-jnp.inf, jnp.inf), perturb_step = 0.2):
     epsilon = length_scale/L
-    key_step, key_accept = jrandom.split(key, 2)
-    initial_momentum = jrandom.normal(key_step, shape = samples.shape)*jnp.sqrt(M)
-    E_grad = jax.vmap(jax.grad(E_dist))
+    key_step, key_perturb, key_accept = jrandom.split(key, 3)
+    try:
+        initial_momentum = jrandom.multivariate_normal(key_step, 
+                                                       mean=jnp.zeros(samples.shape[0]),
+                                                       cov = M)
+    except ValueError:
+        initial_momentum = jrandom.normal(key_step, shape = samples.shape)*jnp.sqrt(M)
     
-    # Leapfrog integration
+    E_grad = jax.vmap(jax.grad(E_dist))
+
     proposal = samples
     momentum = initial_momentum
     for i in range(L):
+        step_size = epsilon*(1+perturb_step*jax.random.uniform(key_perturb))
         momentum = momentum - 0.5*epsilon*E_grad(proposal)
         proposal = proposal + epsilon*momentum/M
         momentum = momentum - 0.5*epsilon*E_grad(proposal)
-    log_alpha = (E_dist(samples)+0.5*initial_momentum**2/M) \
-        - (E_dist(proposal)+0.5*momentum**2/M)
+
+    # Metropolis-Hastings acceptance/rejection
+    log_alpha = (E_dist(samples)+0.5*jnp.einsum('i...->i',initial_momentum**2)/M) \
+        - (E_dist(proposal)+0.5*jnp.einsum('i...->i',momentum**2)/M)
     alpha = jnp.exp(jnp.minimum(log_alpha,0.))
     accept = jrandom.uniform(key_accept, minval=0, maxval=1, shape = alpha.shape)<alpha
+
+    # Enforce bound constraints
+    out_of_bounds = jnp.logical_or(proposal>bounds[1],
+                                   proposal<bounds[0])
+    if len(samples.shape)>1:
+        out_of_bounds = jnp.any(out_of_bounds, axis=1)
+    accept = jnp.where(out_of_bounds, False, accept)
+    if len(samples.shape)>1:
+        accept = jnp.repeat(accept[:,jnp.newaxis],samples.shape[1],axis=1)
+
     samples = jnp.where(accept, proposal, samples)
-    return samples
-
-def evolve_chains(key, E_dist, samples, length_scale, n_steps, sampler, **kwargs):
-    samplers = {
-        'rwm': step_rwm,
-        'mala': step_mala,
-        'hmc': step_hmc
-        }
-    step_fn = samplers[sampler]
-
-    for i in range(n_steps):
-        key, key_temp = jrandom.split(key, 2)
-        samples = step_fn(key_temp, E_dist, samples, length_scale, **kwargs)
-        del key_temp
 
     return samples
-
-if __name__=="__main__":
-
-    key = jrandom.key(1)
-
-    n_points = 10000
-    def logp(x):
-        return -x**2/2-0.5*jnp.log(2*jnp.pi)
-
-    key, key_temp = jrandom.split(key, 2)
-    samples = jrandom.normal(key=key_temp, shape=(n_points,))
-    del key_temp
-
-    sample_variance = jnp.var(samples)
-
-    @jax.jit
-    def corr(init_samples, samples):
-        return jnp.mean((init_samples-jnp.mean(init_samples))*(samples-jnp.mean(samples)))
-
-    # HMC
-
-    hmc_samples = samples
-    hmc_corr = [corr(samples, hmc_samples)]
-
-    while hmc_corr[-1]/sample_variance>1/jnp.sqrt(n_points):
-        key, key_temp = jrandom.split(key, 2)
-        hmc_samples = step_hmc(key_temp, lambda x: -logp(x), hmc_samples, jnp.pi/2, L=1)
-        del key_temp
-        hmc_corr.append(corr(samples, hmc_samples))
-
-    # MALA
-
-    mala_samples = samples
-    mala_corr = [corr(samples, mala_samples)]
-
-    while mala_corr[-1]/sample_variance>1/jnp.sqrt(n_points):
-        key, key_temp = jrandom.split(key, 2)
-        mala_samples = step_mala(key_temp, lambda x: -logp(x), mala_samples, 2.15)
-        del key_temp
-        mala_corr.append(corr(samples, mala_samples))
-
-    # RWM
-
-    rwm_samples = samples
-    rwm_corr = [corr(samples, rwm_samples)]
-
-    while rwm_corr[-1]/sample_variance>1/jnp.sqrt(n_points):
-        key, key_temp = jrandom.split(key, 2)
-        rwm_samples = step_rwm(key_temp, lambda x: -logp(x), rwm_samples, 2.0)
-        del key_temp
-        rwm_corr.append(corr(samples, rwm_samples))
-    
-
-    fig, axes = plt.subplots(1, 2, figsize=(12,4))
-
-    axes[0].plot(rwm_corr,label='RWM')
-    axes[0].plot(mala_corr,label='MALA')
-    axes[0].plot(hmc_corr,label='HMC')
-    axes[0].legend()
-    axes[0].set_ylim(1/jnp.sqrt(n_points), 1)
-    axes[0].set_yscale('log')
-    axes[0].set_xlabel('Steps')
-    axes[0].set_ylabel('Autocorrelation')
-
-    bins = jnp.linspace(-5,5,101)
-    axes[1].hist(rwm_samples, bins=bins, density=True, alpha=0.25,label='RWM')
-    axes[1].hist(mala_samples, bins=bins, density=True, alpha=0.25,label='MALA')
-    axes[1].hist(hmc_samples, bins=bins, density=True, alpha=0.25,label='HMC')
-    axes[1].hist(samples, bins=bins, density=True, alpha=0.25,label='Initial Samples')
-    axes[1].plot(bins, jnp.exp(logp(bins)))
-    axes[1].legend()
-    axes[1].set_xlabel('Samples')
-    axes[1].set_ylabel('Normalized Counts')
-
-    plt.show()
